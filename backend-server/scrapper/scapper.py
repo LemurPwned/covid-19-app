9a2e99ec8bc3dea7cd959c4bed3459287148ea58
LemurPwned <jakub.mojsiejuk@gmail.com>
2020-03-28 16:11:42 +0100

    isolation hospital scrapping

--- backend-server/scrapper/scapper.py
+++ backend-server/scrapper/scapper.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import bs4
+import json
+
+from collections import defaultdict
 
 
 def fetch_isolation_hospitals():
@@ -12,11 +16,19 @@ def fetch_isolation_hospitals():
                                            {'class': 'law-court__list'})[0]
         specific_list = active_regions_list.find('ul')
         # print(specific_list)
+
+        hospital_list = {}
         for reg in specific_list:
-            if isinstance(reg, BeautifulSoup.element.Tag):
-                print('\n', reg)
-                print(type(reg), dir(reg))
+            if isinstance(reg, bs4.element.Tag):
+                region = reg.find('h3').text
+                hospital_list[region] = []
+                hospitals = reg.findAll('li')
+                for hospital in hospitals:
+                    print(hospital.text)
+                    hospital_list[region].append(hospital.text)
+
             # print(reg, type(reg), dir(reg))
+        json.dump(hospital_list, open("Hospitals.json", 'w'))
 
 
 fetch_isolation_hospitals()
