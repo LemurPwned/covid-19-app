23113ce7788a394e049e28e8022498ab84728880
LemurPwned <jakub.mojsiejuk@gmail.com>
2020-05-13 22:08:26 +0200

    define state as per jamoard -- machine response

---
+++ app/covid_tracker/lib/State.dart
@@ -0,0 +1,35 @@
+
+enum StateType { LOCATION, MULTICHOICE, TEXT, TWITTER }
+
+class UserInput {
+  final String message;
+  List<String> choices;
+
+  UserInput({this.message});
+
+  Map<String, dynamic> toJson() => {'message': message, 'choices': choices};
+
+  UserInput.fromJson(Map<String, dynamic> json):
+    message = json['message'], choices = json['choices']
+}
+
+class MobileState {
+  final StateType state;
+  final String userId;
+  final String timestamp;
+  final UserInput userInput;
+  MobileState({this.state, this.userId, this.timestamp, this.userInput});
+
+  MobileState.fromJson(Map<String, dynamic> json)
+      : state = json['state'],
+        userId = json['userId'],
+        timestamp = json['timestamp'],
+        userInput = json['userInput'];
+
+  Map<String, dynamic> toJson() => {
+        'state': state,
+        'userId': userId,
+        'timestamp': timestamp,
+        'userInput': userInput
+      };
+}
