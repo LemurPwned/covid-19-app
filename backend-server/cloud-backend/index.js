be0dec0544ab7630e15a57d701ebeda294f60dcf
jakbol <jakub.boliglowa@fitech.pl>
2020-05-13 10:18:57 +0200

    refactored code

--- backend-server/cloud-backend/index.js
+++ backend-server/cloud-backend/index.js
@@ -1,13 +1,17 @@
+var DialogFlowClient = require("./DialogFlowClient");
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+const projectId = "newagent-jqwvxl";
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
+
+let result = DialogFlowClient.requestDialogFlow(projectId, DialogFlowClient.cred);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
