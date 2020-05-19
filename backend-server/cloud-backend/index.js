db74ec88784acb6f547929e066ef3a6967963b35
jakbol <jakub.boliglowa@fitech.pl>
2020-05-19 23:59:05 +0200

    added packages.json

--- backend-server/cloud-backend/index.js
+++ backend-server/cloud-backend/index.js
@@ -1,14 +1,7 @@
+'use strict';
 const state = require("./state")
-const express = require('express')
 
-// Create a new instance of express
-const app = express()
-
-// Tell express to use the body-parser middleware and to not parse extended bodies
-// app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.json());
-// Route that receives a POST request to /sms
-app.post('/response', function (req, res) {
+exports.postResponse = (req, res) => {
   const body = req.body
 
   // const jsonBody = JSON.parse(body);
@@ -18,13 +11,4 @@ app.post('/response', function (req, res) {
       req.statusCode(404).send(err)
     }
   )
-})
-
-// Tell our app to listen on port 3000
-app.listen(3000, function (err) {
-  if (err) {
-    throw err
-  }
-
-  console.log('Server started on port 3000')
-})
+};
---
+++ backend-server/cloud-backend/localIndex.js
@@ -0,0 +1,30 @@
+const state = require("./state")
+const express = require('express')
+
+// Create a new instance of express
+const app = express()
+
+// Tell express to use the body-parser middleware and to not parse extended bodies
+// app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json());
+// Route that receives a POST request to /sms
+app.post('/response', function (req, res) {
+  const body = req.body
+
+  // const jsonBody = JSON.parse(body);
+  state.uponStateRequest(body, res).catch(
+    (err) => {
+      console.log(`Error ${err}`)
+      req.statusCode(404).send(err)
+    }
+  )
+})
+
+// Tell our app to listen on port 3000
+app.listen(3000, function (err) {
+  if (err) {
+    throw err
+  }
+
+  console.log('Server started on port 3000')
+})
