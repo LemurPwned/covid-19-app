6987aafaf17eb776488f4535d5f1bcc69b06c5e3
LemurPwned <jakub.mojsiejuk@gmail.com>
2020-06-16 22:51:01 +0200

    adding weather handling for dialogflow response

--- backend-server/cloud-backend/state.js
+++ backend-server/cloud-backend/state.js
@@ -1,6 +1,7 @@
 const dfClient = require('./dialogFlowClient')
 const model = require('./algo')
 const fs = require('fs');
+const request = require('request');
 
 
 const Actions = {
@@ -17,6 +18,7 @@ const Intents = {
     NotWellIntent: "NotWellIntent",
     EverythingIsFineIntent: "EverythingIsFineIntent",
     SomethingIsNotFineIntent: "SomethingIsWrongIntnet",
+    WeatherIntent: "weather"
 }
 
 let allSymptoms = ['fever', 'cough', 'general malaise', 'throat pain',
@@ -153,6 +155,37 @@ function onDefaultState(body, dfResponse, res) {
     res.json(responseState)
 }
 
+
+function onWeatherState(body, dfResponse, res) {
+    /**
+     * Run OpenWeatherAPI 
+     */
+    let city = JSON.parse(dfResponse.fulfillmentText).address.trim()
+    let query = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4526d487f12ef78b82b7a7d113faea64`
+    request(query, { json: true }, (err, response, body) => {
+        if (err) {
+            console.log(err)
+            res.status(404).json({ "error": err });
+            return
+        } else {
+            let weatherText = `Weather in ${city}: ${body["weather"][0]["main"]} -- ${body["weather"][0]["description"]}`
+            weatherText += `\nTemperature: ${body["main"]["temp"]}F!`
+            console.log(weatherText)
+            let responseState = formulateState(
+                Actions.Text,
+                dfResponse.intent.displayName,
+                weatherText,
+                null,
+                false
+            )
+            res.status(200).set('Content-Type', 'application/json')
+            res.json(responseState)
+        }
+    });
+}
+
+
+
 async function uponStateRequest(mobileRequest, res) {
     /**
      *  Mobile request defines a state 
@@ -182,10 +215,14 @@ async function uponStateRequest(mobileRequest, res) {
                 console.log("Not well response")
                 onNotWellIntent(mobileRequest, dfResponse, res)
                 break
+            case Intents.WeatherIntent:
+                console.log("Weather intent")
+                onWeatherState(mobileRequest, dfResponse, res)
+                break
             default:
                 onDefaultState(mobileRequest, dfResponse, res)
                 console.log("Default response")
-
+                break
         }
     }
 
